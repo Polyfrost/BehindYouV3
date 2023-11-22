2ef86f5d333f10fbab5ec03462c9df3bc30c023b
Wyvest <wyvestbusiness@gmail.com>
2023-11-22 09:38:16 +0900

    update PGT

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.6.10" apply false
-    id("net.kyori.blossom") version "1.3.0" apply false
+    kotlin("jvm") version "1.8.22" apply false
+    id("org.polyfrost.multi-version.root")
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("cc.polyfrost.multi-version.root")
 }
 
 preprocess {
     "1.12.2-forge"(11202, "srg") {
-        "1.8.9-forge"(10809, "srg", file("versions/1.12.2-1.8.9.txt"))
+        "1.8.9-forge"(10809, "srg")
     }
 }
