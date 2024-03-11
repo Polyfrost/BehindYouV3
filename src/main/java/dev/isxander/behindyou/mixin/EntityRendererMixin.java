8dbfdd57a0be1d6e62cd66c00ce8280d89cad1c3
Wyvest <wyvestbusiness@gmail.com>
2024-03-11 14:11:36 +0700

    fix chunk unloading issues in first person

--- src/main/java/dev/isxander/behindyou/mixin/EntityRendererMixin.java
+++ src/main/java/dev/isxander/behindyou/mixin/EntityRendererMixin.java
@@ -2,6 +2,7 @@ package dev.isxander.behindyou.mixin;
 
 import dev.isxander.behindyou.BehindYou;
 import dev.isxander.behindyou.config.BehindYouConfig;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.settings.GameSettings;
 import org.spongepowered.asm.mixin.*;
@@ -9,6 +10,8 @@ import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
+
+    @Unique private float behindyou$prevLevel = 0;
 
     @Unique
     private boolean behindYouV3$enable() {
@@ -33,7 +36,15 @@ public class EntityRendererMixin {
     @ModifyArg(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;translate(FFF)V", ordinal = 2), index = 2)
     private float set(float z) {
         BehindYou.INSTANCE.setDistance(-z);
-        return behindYouV3$enable() ? -BehindYou.INSTANCE.level() : z;
+        if (behindYouV3$enable()) {
+            float level = -BehindYou.INSTANCE.level();
+            if (behindyou$prevLevel != level) {
+                behindyou$prevLevel = level;
+                Minecraft.getMinecraft().renderGlobal.setDisplayListEntitiesDirty();
+            }
+            return level;
+        }
+        return z;
     }
 
 }
