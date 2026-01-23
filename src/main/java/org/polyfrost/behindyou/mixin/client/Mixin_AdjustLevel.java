3a786216375044bef519430d7ff926ef151b53d2
cxntered <me@cxntered.dev>
2026-01-23 19:55:30 +1100

    ♻️ refactor: use mixinextras
    fixes mod toggling

--- src/main/java/org/polyfrost/behindyou/mixin/client/Mixin_AdjustLevel.java
+++ src/main/java/org/polyfrost/behindyou/mixin/client/Mixin_AdjustLevel.java
@@ -1,16 +1,22 @@
 package org.polyfrost.behindyou.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.deftu.omnicore.api.client.render.OmniRenderTicks;
 import net.minecraft.client.Camera;
 import org.polyfrost.behindyou.client.BehindYouClient;
+import org.polyfrost.behindyou.client.BehindYouConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Camera.class)
 public class Mixin_AdjustLevel {
-    @Redirect(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getMaxZoom(F)F"))
-    private float adjustLevel(Camera instance, float zoom) {
-        return (float) BehindYouClient.getLevel(zoom, OmniRenderTicks.get());
+    @WrapOperation(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getMaxZoom(F)F"))
+    private float adjustLevel(Camera instance, float zoom, Operation<Float> original) {
+        if (BehindYouConfig.INSTANCE.isEnabled()) {
+            return (float) BehindYouClient.getLevel(zoom, OmniRenderTicks.get());
+        } else {
+            return original.call(instance, zoom);
+        }
     }
 }
--- src/main/java/org/polyfrost/behindyou/mixin/client/Mixin_CapturePOVSet.java
+++ src/main/java/org/polyfrost/behindyou/mixin/client/Mixin_CapturePOVSet.java
@@ -1,19 +1,25 @@
 package org.polyfrost.behindyou.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.deftu.omnicore.api.client.options.OmniPerspective;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
 import org.polyfrost.behindyou.client.BehindYouClient;
+import org.polyfrost.behindyou.client.BehindYouConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Minecraft.class)
 public class Mixin_CapturePOVSet {
-    @Redirect(method = "handleKeybinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Options;setCameraType(Lnet/minecraft/client/CameraType;)V"))
-    private void capturePOVSetF5(Options instance, CameraType arg) {
-        OmniPerspective perspective = OmniPerspective.from(arg.ordinal());
-        BehindYouClient.updatePerspective(perspective);
+    @WrapOperation(method = "handleKeybinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Options;setCameraType(Lnet/minecraft/client/CameraType;)V"))
+    private void capturePOVSetF5(Options instance, CameraType arg, Operation<Void> original) {
+        if (BehindYouConfig.INSTANCE.isEnabled()) {
+            OmniPerspective perspective = OmniPerspective.from(arg.ordinal());
+            BehindYouClient.updatePerspective(perspective);
+        } else {
+            original.call(instance, arg);
+        }
     }
 }
