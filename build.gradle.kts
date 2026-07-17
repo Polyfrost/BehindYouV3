797ca0ebd3d48295e75a982e8dcbdff6c3c2c9df
Luna <lunaynx@gmail.com>
2026-07-17 03:03:33 +0200

    build: fix parchment
    [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
     maven("https://repo.polyfrost.org/releases")
     maven("https://repo.polyfrost.org/snapshots")
     maven("https://maven.parchmentmc.org") {
-        content { includeGroup("org.parchmentmc") }
+        content { includeGroupAndSubgroups("org.parchmentmc") }
     }
     maven("https://maven.gegy.dev/releases") {
         content { includeGroup("dev.lambdaurora") }
