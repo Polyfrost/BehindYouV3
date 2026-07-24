26638da4a1b44e15a5d36fffab19005c4eb51f65
Luna <lunaynx@gmail.com>
2026-07-24 20:19:41 +0200

    test: enable full stack trace

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -93,6 +94,10 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        showStackTraces = true
+        exceptionFormat = TestExceptionFormat.FULL
+    }
 }
 
 bloom {
