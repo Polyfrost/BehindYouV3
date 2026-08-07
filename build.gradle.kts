40a760d95ad8a5338ef7f5066c8f32ac1103326c
Luna <lunaynx@gmail.com>
2026-08-07 12:26:34 +0000

    build: switch back to official TerraformersMC repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,11 +43,8 @@ repositories {
     maven("https://maven.fabricmc.net/releases") {
         content { includeGroup("net.fabricmc") }
     }
-    //maven("https://maven.terraformersmc.com/releases") {
-    maven("https://maven.gnomecraft.net/releases/") {
-        content {
-            includeGroup("com.terraformersmc")
-        }
+    maven("https://maven.terraformersmc.com/") {
+        content { includeGroup("com.terraformersmc") }
     }
 }
 
